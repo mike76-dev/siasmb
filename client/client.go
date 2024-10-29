1d9730eafe5f0f33b3ce7ea9c4fc7d57b11f9b28
mike76-dev <michael.bulanov@yahoo.com>
2024-10-29 21:12:19 +0100

    Leverage `renterd` API

--- client/client.go
+++ client/client.go
@@ -1,9 +1,12 @@
 package client
 
 import (
+	"context"
 	"fmt"
+	"strings"
 
 	"go.sia.tech/jape"
+	"go.sia.tech/renterd/api"
 )
 
 type Client struct {
@@ -17,12 +20,66 @@ func New(addr, password string) *Client {
 	}}
 }
 
-type Bucket struct {
-	Name      string `json:"name"`
-	CreatedAt string `json:"createdAt"`
+func (c *Client) GetBucket(ctx context.Context, name string) (bucket api.Bucket, err error) {
+	err = c.c.WithContext(ctx).GET(fmt.Sprintf("/api/bus/bucket/%s", name), &bucket)
+	return
 }
 
-func (c *Client) GetBucket(name string) (bucket Bucket, err error) {
-	err = c.c.GET(fmt.Sprintf("/api/bus/bucket/%s", name), &bucket)
+func (c *Client) GetObject(ctx context.Context, bucket, path string) (obj api.ObjectsResponse, err error) {
+	path = api.ObjectPathEscape(path)
+	path += "?bucket=" + bucket
+	err = c.c.WithContext(ctx).GET(fmt.Sprintf("/api/bus/objects/%s", path), &obj)
 	return
+}
+
+func (c *Client) GetObjectInfo(ctx context.Context, bucket, path string) (info api.ObjectMetadata, err error) {
+	var resp api.ObjectsResponse
+	if path == "" {
+		var b api.Bucket
+		b, err = c.GetBucket(ctx, bucket)
+		if err != nil {
+			return
+		}
+
+		resp, err = c.GetObject(ctx, bucket, "")
+		if err != nil {
+			return
+		}
+
+		var size int64
+		for _, obj := range resp.Entries {
+			size += obj.Size
+		}
+
+		info = api.ObjectMetadata{
+			ETag:    "",
+			ModTime: b.CreatedAt,
+			Name:    "/",
+			Size:    size,
+		}
+
+		return
+	}
+
+	var parentDir string
+	i := strings.LastIndex(path, "/")
+	if i < 0 {
+		parentDir = ""
+	} else {
+		parentDir = path[:i+1]
+	}
+
+	resp, err = c.GetObject(ctx, bucket, parentDir)
+	if err != nil {
+		return
+	}
+
+	for _, obj := range resp.Entries {
+		if obj.Name == "/"+path || obj.Name == "/"+path+"/" {
+			info = obj
+			return
+		}
+	}
+
+	return api.ObjectMetadata{}, api.ErrObjectNotFound
 }
--- go.mod
+++ go.mod
@@ -1,15 +1,32 @@
 module github.com/mike76-dev/siasmb
 
-go 1.21.7
+go 1.21.8
 
 toolchain go1.22.8
 
 require (
+	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
 	github.com/geoffgarside/ber v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gotd/contrib v0.20.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
+	github.com/klauspost/reedsolomon v1.12.1 // indirect
+	gitlab.com/NebulousLabs/errors v0.0.0-20200929122200-06c536cf6975 // indirect
+	gitlab.com/NebulousLabs/fastrand v0.0.0-20181126182046-603482d69e40 // indirect
+	go.sia.tech/core v0.3.0 // indirect
 	go.sia.tech/jape v0.12.1 // indirect
+	go.sia.tech/mux v1.2.0 // indirect
+	go.sia.tech/renterd v1.0.8 // indirect
+	go.sia.tech/siad v1.5.10-0.20230228235644-3059c0b930ca // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
-	golang.org/x/tools v0.5.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.10 // indirect
+	lukechampine.com/frand v1.4.2 // indirect
 )
